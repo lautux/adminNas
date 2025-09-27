f85aeaa9b19aa038ed26640835403e9254f746d2
lautux76@gmail.com <lautux76@gmail.com>
2025-09-27 18:46:04 +0200

    params

--- nasReport.py
+++ nasReport.py
@@ -6,63 +6,69 @@
 import logging
 import config
 import sys
+import argparse
 from classes.logger import Logger
 from classes.raid import Raid
 from classes.smart import Smart
 from classes.fail2ban import Fail2ban
 from classes.cpu import Cpu
-from classes.param import Param
-
-#log = Logger(f"{config.LOG_FILE_PATH}", config.LOGGING_MODE)
-log = Logger(f"{config.LOG_FILE_PATH}", logging.WARNING)
-
-print(sys.argv)
-
-print(f"{'-'*40}")
-print(f"{'Status du NAS': ^{40}}")
-print(f"{'-'*40}")
 
 
-###############################################################################
-# RAID devices
-###############################################################################
-raid = Raid(config.RAID_PATHS, log)
-raid_globalStatus = raid.getGlobalStatus()
-print(f"\n # Etat des RAID : {'OK' if raid_globalStatus else 'KO'}")
-if not raid_globalStatus:
-    print(raid.getGlobalDetails())
+def main():
+    parser = argparse.ArgumentParser(description="Script d'analyse du NAS")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Verbose mode")
+    parser.add_argument("-d", "--details", action="store_true", help="Show more details")
+    args = parser.parse_args()
+    log = Logger(f"{config.LOG_FILE_PATH}", logging.DEBUG if args.verbose else config.LOGGING_MODE)
+
+    ###############################################################################
+    # Report header
+    ###############################################################################
+    print(f"{'-'*40}")
+    print(f"{'Status du NAS': ^{40}}")
+    print(f"{'-'*40}")
 
 
-###############################################################################
-# SMART health
-###############################################################################
-smart = Smart(config.HDD_PATHS, log)
-smart_globalStatus = smart.getGlobalStatus()
-print(f"\n # Santé des disques : {'OK' if smart_globalStatus else 'KO'}")
-if not smart_globalStatus:
-    print(smart.getGlobalDetails())
+    ###############################################################################
+    # RAID devices
+    ###############################################################################
+    raid = Raid(config.RAID_PATHS, log)
+    raid_globalStatus = raid.getGlobalStatus()
+    print(f"\n # Etat des RAID : {'OK' if raid_globalStatus else 'KO'}")
+    if not raid_globalStatus or args.details:
+        print(raid.getGlobalDetails())
 
 
-###############################################################################
-# Fail2ban service status
-###############################################################################
-fail2ban = Fail2ban(log)
-fail2ban_globalStatus = fail2ban.getGlobalStatus()
-fail2ban_ip = fail2ban.getBannedIp()
-print(f"\n # Status de Fail2ban : {'OK' if fail2ban_globalStatus else 'KO'}")
-if not fail2ban_globalStatus:
-    print(fail2ban.getGlobalDetails())
-if fail2ban_ip != "":
-    print(f"Banned IP : {fail2ban_ip}")
+    ###############################################################################
+    # SMART health
+    ###############################################################################
+    smart = Smart(config.HDD_PATHS, log)
+    smart_globalStatus = smart.getGlobalStatus()
+    print(f"\n # Santé des disques : {'OK' if smart_globalStatus else 'KO'}")
+    if not smart_globalStatus or args.details:
+        print(smart.getGlobalDetails())
 
 
-###############################################################################
-# CPU status
-###############################################################################
-"""cpu = Cpu(log)
-cpu_globalStatus = cpu.getGlobalStatus()
-print(f"\n # Utilisation du CPU : {'OK' if cpu_globalStatus else 'KO'}")
-#cpu_globalStatus = False
-if not cpu_globalStatus:
-    print(f"\t/!\\ Vérifier l'utilisation du CPU : {' '.join(cpu.checkCommand)}")"""
+    ###############################################################################
+    # Fail2ban service status
+    ###############################################################################
+    fail2ban = Fail2ban(log)
+    fail2ban_globalStatus = fail2ban.getGlobalStatus()
+    fail2ban_ip = fail2ban.getBannedIp()
+    print(f"\n # Status de Fail2ban : {'OK' if fail2ban_globalStatus else 'KO'}")
+    if not fail2ban_globalStatus or args.details:
+        print(fail2ban.getGlobalDetails())
+    if fail2ban_ip != "":
+        print(f"Banned IP : {fail2ban_ip}")
+
+
+    ###############################################################################
+    # CPU status
+    ###############################################################################
+    """cpu = Cpu(log)
+    cpu_globalStatus = cpu.getGlobalStatus()
+    print(f"\n # Utilisation du CPU : {'OK' if cpu_globalStatus else 'KO'}")
+    #cpu_globalStatus = False
+    if not cpu_globalStatus:
+        print(f"\t/!\\ Vérifier l'utilisation du CPU : {' '.join(cpu.checkCommand)}")"""
 
