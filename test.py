df4c50e90ea0432384f97d947f2d6f4510f3101a
lautux76@gmail.com <lautux76@gmail.com>
2025-09-27 18:47:54 +0200

    params

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ from classes.mail import Mail
 log = Logger(f"{config.LOG_FILE_PATH}", config.LOGGING_MODE)
 
 resultat = subprocess.run(
-    ["python3", "nasReport.py", "arg1", "arg2"],
+    ["python3", "nasReport.py", "-d"],
     capture_output=True,
     text=True
 )
