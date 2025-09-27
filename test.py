4f9e28f998ca11f5efabb4eaaf127d0025bc73d0
lautux76@gmail.com <lautux76@gmail.com>
2025-09-27 19:26:47 +0200

    test debug mode

--- test.py
+++ test.py
@@ -17,6 +17,11 @@ def exec(tabCmd):
         print(f"error :\n*{resultat.stderr}*")
 
 exec(["python3", "nasReport.py"])
+log.debug(f"{'#'*20} DEBUG {'#'*20}")
+log.info(f"{'#'*20} INFO {'#'*20}")
+log.warning(f"{'#'*20} WARNING {'#'*20}")
+log.error(f"{'#'*20} ERROR {'#'*20}")
+log.critical(f"{'#'*20} CRITICAL {'#'*20}")
 exec(["python3", "nasReport.py", "-v"])
 #exec(["python3", "nasReport.py", "-d", "-v"])
 
