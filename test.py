2c4caba12de0080a461515623459420ce0316b70
lautux76@gmail.com <lautux76@gmail.com>
2025-09-27 18:55:00 +0200

    a

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ from classes.mail import Mail
 log = Logger(f"{config.LOG_FILE_PATH}", config.LOGGING_MODE)
 
 resultat = subprocess.run(["python3", "nasReport.py", "-d"], capture_output=True, text=True)
-print(f"{resultat.stdout}{"error :"+resultat.stderr if not resultat.stderr else ""}")
+print(f"{resultat.stdout}{("error :"+resultat.stderr) if not resultat.stderr else ""}")
 
 """mail = Mail(log)
 print("mail.send() - DEBUT")
