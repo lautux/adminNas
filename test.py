e4f0c1c044aa756d96df3c9c7b72b185046369dc
lautux76@gmail.com <lautux76@gmail.com>
2025-09-27 18:20:29 +0200

    test1

--- test.py
+++ test.py
@@ -2,13 +2,24 @@
 
 import logging
 import config
+import sys
+import subprocess
 from classes.logger import Logger
 from classes.mail import Mail
 
 log = Logger(f"{config.LOG_FILE_PATH}", config.LOGGING_MODE)
-mail = Mail(log)
+
+resultat = subprocess.run(
+    ["python", "nasReport.py", "arg1", "arg2"],
+    capture_output=True,
+    text=True
+)
+print("Sortie :", resultat.stdout)
+print("Erreur :", resultat.stderr)
+
+"""mail = Mail(log)
 print("mail.send() - DEBUT")
 mail.send("lautux76@gmail.com", "test1", "bla bla bla")
-print("mail.send() - FIN")
+print("mail.send() - FIN")"""
 
 
